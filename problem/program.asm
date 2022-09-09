31ffe46f8334277c2c84ce79d9c29dfb6ed989a6
EliteV1 <elitemoonknight@gmail.com>
2022-09-08 19:51:42 -0500

    Posible commit final

--- problem/program.asm
+++ problem/program.asm
@@ -3,6 +3,28 @@
 // emanuel.perez@.edu.co
 
 (START)			
+	@16400
+    D=A
+
+    @CordScr  //Scr= screen
+	M=D 
+	@256
+	D=A
+
+	@contV
+	M=D
+
+	@32
+	D=A
+	@contH
+	M=D
+
+	@4096
+	D=A
+	@starting
+	M=D
+
+	
 	@KBD
 	D=M
 	@KEY	
@@ -11,26 +33,59 @@
 	0;JMP
 
 (KEY)
-// 84 para la T, 67 para la c
+	// 84 = T, 67 = c
 	@j
-	M = D // save key
-	@84  // press f
+	M = D 
+	@84  //T
 	D = D-A
-	@FILL
+	@LV
 	D;JEQ
 	@j
 	D = M
-	@67
+	@67 //C
 	D = D - A
 	@ERASE
 	D;JEQ
 	@START
 	0;JMP
 
-(FILL)
-	@value
-	M = -1
-	@DRAW
+(LV)
+	@contV
+	D=M
+	@LH
+	D;JEQ
+
+	@256
+	D=A
+	@CordScr
+	A=M
+	M=D
+	@contV
+	M=M-1
+	@32
+	D=A
+	@CordScr
+	M=M+D
+	@LV
+	0;JMP
+	//@LH
+
+(LH)
+	@starting
+	D=M
+	@SCREEN
+	A=A+D
+	M=-1
+
+	@starting
+	M=M+1
+
+	@contH
+	M=M-1
+	D=M
+	@KEY
+	D;JEQ
+	@LH
 	0;JMP
 
 (ERASE)
